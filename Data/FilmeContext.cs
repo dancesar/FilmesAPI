74a886ff28bcfe8459b0105f595c421b0f50d84d
dancesar <danillo.yamagush@gmail.com>
2022-05-23 18:55:30 -0300

    Conectando ao Banco de Dados

---
+++ Data/FilmeContext.cs
@@ -0,0 +1,17 @@
+using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class FilmeContext : DbContext
+    {
+
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base (opt)
+        {
+            
+        }
+
+        public DbSet<Filmes> Filmes { get; set; }
+        
+    }
+}
